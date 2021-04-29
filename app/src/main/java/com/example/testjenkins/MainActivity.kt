abfb96596973f3fc193f5b41db3fd2ea6f209d04
qe519438 <39982312+qe519438@users.noreply.github.com>
2021-04-29 15:57:49 +0800

    Update

--- app/src/main/java/com/example/testjenkins/MainActivity.kt
+++ app/src/main/java/com/example/testjenkins/MainActivity.kt
@@ -10,5 +10,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         Log.d("ttttt", "ttttt")
         Log.d("ttttt", "ttttt")
+        Log.d("ttttt", "ttttt")
     }
 }

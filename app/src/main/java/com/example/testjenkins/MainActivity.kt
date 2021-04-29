608491212fa3fe13f460cc2c83e53126291b6460
qe519438 <39982312+qe519438@users.noreply.github.com>
2021-04-29 17:34:05 +0800

    Update

--- app/src/main/java/com/example/testjenkins/MainActivity.kt
+++ app/src/main/java/com/example/testjenkins/MainActivity.kt
@@ -11,5 +11,6 @@ class MainActivity : AppCompatActivity() {
         Log.d("ttttt", "ttttt")
         Log.d("ttttt", "ttttt")
         Log.d("ttttt", "ttttt")
+        Log.d("ttttt", "ttttt")
     }
 }
